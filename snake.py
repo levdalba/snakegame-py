b1e1722e0d5ea90d2d4cb5a6e94e70b7393bf71c
levdalba <levandalbashvili9@gmail.com>
2023-10-09 09:57:07 +0700

    create a 3 length bar

--- snake.py
+++ snake.py
@@ -1,9 +1,19 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 
 screen.bgcolor("black")
 
 screen.setup(width=600, height=600)
+
+segments = []
+
+positions = [(0, 0), (20, 0), (40, 0)]
+for pos in positions:
+    new_turtle = Turtle()
+    new_turtle.shape("square")
+    new_turtle.color("white")
+    new_turtle.goto(pos)
+    segments.append(new_turtle)
 
 screen.exitonclick()
